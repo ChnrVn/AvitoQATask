c28363411be7957a2f5a54bd172081cfecf3ad9c
ChnrVn <chunarchunar@outlook.com>
2023-08-31 21:54:37 +0300

    2

--- src/test/java/org/example/FavoriteTest.java
+++ src/test/java/org/example/FavoriteTest.java
@@ -22,6 +22,7 @@ class FavoriteTest {
 
         driver.get(TEST_URL);
 
+        //Поиск кнопки "Избранное"
         WebElement webElement = driver.findElements(
                         By.xpath("//button[@data-marker='item-view/favorite-button']"))
                 .stream()
@@ -32,23 +33,25 @@ class FavoriteTest {
 
         webElement.click();
 
+        //Дождаться пока кнопка сменит состояние
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.attributeToBe(
                         webElement,
                         "data-is-favorite",
                         "true"));
 
-
         driver.get("https://www.avito.ru/favorites");
 
+        // Найти в списке "избранное" элемент с URL объявления, добавленного в ходе теста
         List<WebElement> references = driver.findElements(By.xpath(
                         "//div[@class='item-snippet-root-d2wFO']//div[@class='item-snippet-column-2-md2mY']//a"))
                 .stream()
                 .filter(webElement1-> TEST_URL.equals(webElement1.getAttribute("href")))
                 .toList();
+
         driver.quit();
 
         Assertions.assertEquals(1, references.size());
-
+        //Такое объявления должно быть ровно одно
     }
 }
