95da684412119b6b5eae54530bd18a90e41521f5
ChnrVn <chunarchunar@outlook.com>
2023-08-31 21:43:39 +0300

    Initial commit

---
+++ src/test/java/org/example/FavoriteTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+
+class FavoriteTest {
+    public static String TEST_URL = "https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363";
+
+    @Test
+    public void addToFavListTest(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(TEST_URL);
+
+        WebElement webElement = driver.findElements(
+                        By.xpath("//button[@data-marker='item-view/favorite-button']"))
+                .stream()
+                .filter(el -> "desktop-usq1f1".equals(el.getAttribute("class")))
+                .findFirst()
+                .orElseThrow();
+
+
+        webElement.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.attributeToBe(
+                        webElement,
+                        "data-is-favorite",
+                        "true"));
+
+
+        driver.get("https://www.avito.ru/favorites");
+
+        List<WebElement> references = driver.findElements(By.xpath(
+                        "//div[@class='item-snippet-root-d2wFO']//div[@class='item-snippet-column-2-md2mY']//a"))
+                .stream()
+                .filter(webElement1-> TEST_URL.equals(webElement1.getAttribute("href")))
+                .toList();
+        driver.quit();
+
+        Assertions.assertEquals(1, references.size());
+
+    }
+}
